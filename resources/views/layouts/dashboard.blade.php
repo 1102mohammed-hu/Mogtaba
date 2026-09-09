bb2ef76be8debad6c4a127a77440a528650ea427
محمد حميد الشرعبي <2003mohammedalsharabi@gmail.com>
2026-09-09 19:20:28 +0300

    first commit

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Dashboard')</title>
+    <link rel="icon" type="images/png" href="{{ asset('profile.png') }}">
 <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- Vite --}}
     @vite([
