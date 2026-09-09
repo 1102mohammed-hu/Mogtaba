f4661e14da452a5f3aa74e2bf7a6b28bb52e48a8
محمد حميد الشرعبي <2003mohammedalsharabi@gmail.com>
2026-09-09 19:07:05 +0300

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,23 +8,15 @@ use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        // توافق الجداول مع MySQL
         Schema::defaultStringLength(191);
 
-        // إجبار Laravel على استخدام HTTPS في الإنتاج
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
