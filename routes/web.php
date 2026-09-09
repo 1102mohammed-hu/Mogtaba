31f305c951bfa995d802494ebcdd11ba492ab006
محمد حميد الشرعبي <2003mohammedalsharabi@gmail.com>
2026-09-09 17:53:56 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -76,4 +76,4 @@ Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 //     return view('components.projects');
 // });
 Route::get('/register', [UserController::class, 'showRegister'])->name('register');
-Route::post('/createUser', [UserController::class, 'register'])->name('register.post');
+Route::post('/register', [UserController::class, 'register'])->name('register.post');
